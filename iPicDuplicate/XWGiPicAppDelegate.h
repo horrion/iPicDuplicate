c4d9b5e5753b6f07382ed557d34a99b543e91f87
Robert Horrion <Robert@Roberts-MacBook-Pro-2.local>
2014-02-14 18:20:09 +0100

    Initial Commit

---
+++ iPicDuplicate/XWGiPicAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XWGiPicAppDelegate.h
+//  iPicDuplicate
+//
+//  Created by Robert Horrion on 2/14/14.
+//  Copyright (c) 2014 Robert Horrion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XWGiPicAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iPicDuplicate/XWGiPicViewController.m
@@ -0,0 +1,29 @@
+//
+//  XWGiPicViewController.m
+//  iPicDuplicate
+//
+//  Created by Robert Horrion on 2/14/14.
+//  Copyright (c) 2014 Robert Horrion. All rights reserved.
+//
+
+#import "XWGiPicViewController.h"
+
+@interface XWGiPicViewController ()
+
+@end
+
+@implementation XWGiPicViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
